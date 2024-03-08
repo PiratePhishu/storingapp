5c9afc143ab68abc0e1e5048dbe1e0179d3d56c3
Phishu <mljm.braxhoofden@gmail.com>
2024-03-08 09:37:47 +0100

    Opdracht af zover ik weet

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -2,18 +2,52 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+if(empty($attractie))
+{
+    $errors[]="Vul de attractie-naam in.";
+}
+
 $capaciteit = $_POST['capaciteit'];
+if(!is_numeric($capaciteit))
+{
+    $errors[]="Vul voor capaciteit een geldig getal in.";
+}
+
 $melder = $_POST['melder'];
+if(empty($melder))
+{
+    $errors[]="Vul uw naam in.";
+}
+
 $type = $_POST['Type'];
+if(empty($type))
+{
+    $errors[]="kies iets uit de lijst.";
+}
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+$overig = $_POST['overig'];
 
+if (isset($_POST['prioriteit']))
+{
+    $prioriteit = 1;
+}
+else
+{
+    $prioriteit = 0;
+}
+
+
+if(isset($errors))
+{
+    var_dump($errors);
+    die();
+}
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, type, melder, capaciteit)
-VALUES(:attractie, :type, :melder, :capaciteit)";
+$query = "INSERT INTO meldingen (attractie, type, melder, capaciteit, prioriteit, overige_info)
+VALUES(:attractie, :type, :melder, :capaciteit, :prioriteit, :overig)";
 
 //3. Prepare
 $statement = $conn->prepare($query);
@@ -22,6 +56,9 @@ $statement->execute([
     ":attractie"=>$attractie,
     ":type"=>$type,
     ":melder"=>$melder,
-    ":capaciteit"=>$capaciteit]);
+    ":capaciteit"=>$capaciteit,
+    ":prioriteit"=>$prioriteit,
+    ":overig"=>$overig]);
+    
 
 header("Location: ../../../resources/views/meldingen/index.php?msg=Meldingopgeslagen");
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -42,6 +42,15 @@
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
             </div>
+            <div class="form-group">
+                <label for="prioriteit">Nieuwsbrief</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">Ik wil graag de nieuwsbrief ontvangen</label>
+            </div>
+            <div class="form-group">
+                <label for="overig_info">overige info:</label>
+                <textarea name = "overig" id="overig"class="form-input"rows="4"></textarea>
+            </div>
 
             <input type="submit" value="Verstuur melding">
 
